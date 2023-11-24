409ac6a0bb6675d38883998bcf283093189a34c4
WedleySilva <WedleySilva2105@gmail.com>
2023-11-24 15:13:27 -0300

    fix(App.vue): comentando background

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,21 @@ import MainComp from './components/MainComp.vue'
 </script>
 
 <template>
-  <div class="img-background ">
+  <!-- <div class="img-background "> -->
   <header>
     <HeaderComp />
   </header>
   <main>
     <MainComp />
   </main>
-  </div>
+  <!-- </div> -->
 </template>
 
 <style scoped>
-html, body {
-  margin: 0;
-  padding: 0;
-}
-.img-background {
+/* .img-background {
   height: 100vh; 
   background: linear-gradient(to bottom right, #75d1ff 50%, transparent 50%);
   overflow: hidden;
   margin: 0;
-}
+} */
 </style>
