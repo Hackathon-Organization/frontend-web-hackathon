8890c5170f71d11b4d99ca07004251fd45a5b58b
WedleySilva <WedleySilva2105@gmail.com>
2023-11-24 11:59:06 -0300

    create(HeaderComp): criação user-badge

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,15 +1,28 @@
 <template>
   <header class="custom-header">
     <h1>Header</h1>
+    <div class="user-badge"></div>
   </header>
 </template>
 
 <style scoped>
+
 .custom-header {
-  background-color: rgb(73, 38, 73);
+  text-align: center;  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  background-color: #492649;
   padding: 20px;
   border-radius: 10px;
   color: #ffffff;
-  text-align: center;
+}
+
+.user-badge {
+  width: 100px;
+  height: 100px;
+  background-color: #27ae60;
+  border-radius: 50%;
 }
 </style>
