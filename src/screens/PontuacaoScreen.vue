fa876ce9d4a855841536da7df500909cf94f8857
wedleysilva <wedleysilva2105@gmail.com>
2023-11-25 09:28:05 -0300

    fix(Pontuacaoscreen): botão recolocado

--- src/screens/PontuacaoScreen.vue
+++ src/screens/PontuacaoScreen.vue
@@ -3,34 +3,36 @@ import { ref, onMounted } from 'vue';
 import api from '@/plugins/axios';
 import Loading from 'vue-loading-overlay';
 
-const participantes = ref([]);
+const equipes = ref([]);
 const isLoading = ref(false);
 
 onMounted(async () => {
   isLoading.value = true;
-  const response = await api.get('/participantes');
-  participantes.value = response.data.results;
-  participantes.value.sort((a, b) => b.pontuacao - a.pontuacao);
+  const response = await api.get('/equipes');
+  equipes.value = response.data.results;
+  equipes.value.sort((a, b) => b.pontuacao - a.pontuacao);
 
   isLoading.value = false;
 });
 </script>
 
 <template>
-  <loading :active="isLoading" :can-cancel="true" is-full-page="false" loader="bars"/>
+  <loading :active="isLoading" :can-cancel="true" is-full-page="false" loader="bars" />
   <div class="pontuacoes-container">
+
+
     <div class="pontuacoes-main">
+      <router-link to="/"><button class="voltar">Voltar</button></router-link>
+
       <div class="titulo-pontuacoes">
         <h1>Pontuações</h1>
       </div>
-      <div class="botao-voltar">
-        <router-link to="/"><button class="voltar">Voltar</button></router-link>
-      </div>
       <div class="nomes-container">
-        <div class="nome-item" v-for="participante in participantes" :key="participante.id">
-          <h2>{{ participante.nome }}</h2> 
-          <h3>Nota: {{ participante.pontuacao }}</h3>
+        <div class="nome-item" v-for="equipe in equipes" :key="equipe.id">
+          <h2>{{ equipe.nome }}</h2>
+          <h3>Nota: {{ equipe.pontuacao }}</h3>
         </div>
+
       </div>
       <div class="botao-avaliar">
       </div>
@@ -38,67 +40,61 @@ onMounted(async () => {
   </div>
 </template>
 
-<style>
-.pontuacoes-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 750px;
-}
+<style scoped> .pontuacoes-container {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   height: 750px;
+ }
 
-.pontuacoes-main {
-  width: 800px;
-  height: 500px;
-  color: #fff;
-  background-color: #012030;
-  border-radius: 30px;
-  overflow: hidden;
-  margin-top: 10px;
-}
+ .pontuacoes-main {
+   width: 800px;
+   height: 500px;
+   color: #fff;
+   background-color: #012030;
+   border-radius: 30px;
+   overflow: hidden;
+   margin-top: 10px;
+ }
 
-.titulo-pontuacoes {
-  margin-top: 20px;
-  text-align: center;
-}
+ .titulo-pontuacoes {
+   margin-top: 20px;
+   text-align: center;
+ }
+ .voltar {
+   height: 50px;
+   width: 200px;
+   margin-top: 25px;
+   margin-left: 20px;
+   font-size: 16px;
+   background-color: #75d1ff;
+   color: #fff;
+   border-radius: 15px;
+   transition: background-color 0.3s;
+ }
 
-.botao-voltar {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-}
+ .voltar:hover {
+   background-color: #012030;
+   color: #012030;
+   border-radius: 15px;
+   color: #fff;
+ }
 
-.voltar {
-  height: 50px;
-  width: 200px;
-  font-size: 16px;
-  background-color: #75d1ff;
-  color: #fff;
-  border-radius: 15px;
-  transition: background-color 0.3s;
-}
+ .nomes-container {
+   display: flex;
+   flex-direction: column;
+   margin-top: 20px;
+   overflow-y: auto;
+   color: #000;
+   text-align: center;
+ }
 
-.voltar:hover {
-  background-color: #012030;
-  color: #012030;
-  border-radius: 15px;
-  color: #fff;
-}
-
-.nomes-container {
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-  overflow-y: auto;
-  color: #000;
-  text-align: center;
-}
-
-.nome-item {
-  border: 2px solid #fff;
-  border-radius: 10px;
-  padding: 10px;
-  margin: 5px;
-  font-size: 20px;
-  background-color: #fff;
-}
+ .nome-item {
+   border: 2px solid #fff;
+   border-radius: 10px;
+   padding: 10px;
+   margin: 5px;
+   font-size: 20px;
+   background-color: #fff;
+ }
 </style>
