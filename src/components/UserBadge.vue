6123dfb32c3861823ab8092adec89d419eb1e3c7
WedleySilva <WedleySilva2105@gmail.com>
2023-11-24 21:20:47 -0300

    refactor(UserBadge.vue): organização do código

---
+++ src/components/UserBadge.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div class="user-badge">
+        <img class="perfil-img" src="@/media/logo.png" alt="Perfil">
+      </div>
+    </header>
+</template>
+
+<style scoped>
+.user-badge {
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 50%;
+  overflow: hidden; 
+}
+
+.perfil-img {
+  width: 100%; 
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover; 
+}
+
+</style>
+```
